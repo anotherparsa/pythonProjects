6ccb57d139c8c79f7a6535ee851d490d72f7636b
anotherparsa <anotherparsa@gmail.com>
2025-02-22 03:38:49 +0330

    sending data to templates

--- Django/myproject/mysecondapp/views.py
+++ Django/myproject/mysecondapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def show_account(request, username):
-    return render(request,  "mysecondapp/account.html")
+    return render(request,  "mysecondapp/account.html", context={'username':username})
