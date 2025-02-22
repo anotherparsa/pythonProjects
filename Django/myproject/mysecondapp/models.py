aefc057d650eaeaa4d74c8205911a7d86abcdce4
anotherparsa <anotherparsa@gmail.com>
2025-02-22 23:44:55 +0330

    working with models

--- Django/myproject/mysecondapp/models.py
+++ Django/myproject/mysecondapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=50)
+    name = models.CharField(max_length=25)
+    password = models.CharField(max_length=32)
+    
